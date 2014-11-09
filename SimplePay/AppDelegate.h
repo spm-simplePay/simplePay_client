0e1d0ec2b73465714bc2112801a59938e8c79932
Burak Colakoglu <Colakoglu@hfuopen922.hfu-open.fu.hs-furtwangen.de>
2014-11-09 16:55:41 +0100

    Initial Commit

---
+++ SimplePay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimplePay
+//
+//  Created by Burak Colakoglu on 09.11.14.
+//  Copyright (c) 2014 Colakoglu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
