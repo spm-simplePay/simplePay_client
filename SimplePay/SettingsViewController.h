d6a5773c7ecc8c73d8d804f2b0a17abfc7461551
Burak Colakoglu <Colakoglu@MacBook-Air.local>
2014-11-18 14:38:00 +0100

    20141118_bc_client: Logout-Button und SettingsViewController hinzugefügt

---
+++ SimplePay/SettingsViewController.h
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.h
+//  SimplePay
+//
+//  Created by Burak Colakoglu on 18.11.14.
+//  Copyright (c) 2014 Colakoglu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+
+- (IBAction)logout:(id)sender;
+
+
+@end
