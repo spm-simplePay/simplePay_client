b76461025966275d5c5f83f583648d798404f566
Burak Colakoglu <Colakoglu@MacBook-Air.local>
2014-12-18 09:12:33 +0100

    SIM-62_20141218_bc_client: View für Einstellungen

---
+++ SimplePay/SettingsTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  SettingsTableViewController.h
+//  SimplePay
+//
+//  Created by Burak Colakoglu on 08.12.14.
+//  Copyright (c) 2014 Colakoglu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTableViewController : UITableViewController
+
+- (IBAction)btn_actionLogout:(id)sender;
+
+
+@end
